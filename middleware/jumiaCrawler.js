1c2913c233869af3db5251476ad32741b1ebfed7
Mornach <nou213090168@noun.edu.ng>
2023-10-23 09:34:38 +0100

    push

--- middleware/jumiaCrawler.js
+++ middleware/jumiaCrawler.js
@@ -9,6 +9,12 @@ const jumiaCrawler = async (query) => {
     defaultViewport: null,
   }); */
   const browser = await puppeteer.launch({
+    args:[
+      "--diasable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote"
+    ]
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
