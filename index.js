2b1d76f3b19bc33f554e49a97faba34f64b71843
Mornach <nou213090168@noun.edu.ng>
2023-10-23 09:25:01 +0100

    push

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 //require("./database/mongodb");
 //require("./database/redis");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
